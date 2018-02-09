932c87b450ed73e2ab0cb46e54b3a25d65163e7d
pankaj kumar <pk0846141@gmail.com>
2018-02-09 20:14:31 +0530

    second submisiion

--- spy_details.py
+++ spy_details.py
@@ -1,6 +1,33 @@
-spy = {
-  'name': 'Pankaj',
-  'age': 24,
-  'rating': 4.7,
-  'is_online': True
-}
+# Importing datetime to display the chat time and date
+from datetime import datetime
+
+# made a class for spy
+class Spy:
+
+  def __init__(self, name, salutation, age, rating):
+    self.name = name
+    self.salutation = salutation
+    self.age = age
+    self.rating = rating
+    self.is_online = True
+    self.chats = []
+    self.current_status_message = None
+
+
+# a class for chat_messages
+class ChatMessage:
+
+    def __init__(self, message, sent_by_me):
+        self.message = message
+        self.time = datetime.now()
+        self.sent_by_me = sent_by_me
+
+
+spy = Spy('Pankaj', 'Mr.', 22, 5.0)
+
+friend_one = Spy('Pranav', 'Mr.', 27, 4.9)
+friend_two = Spy('sumit', 'Mr.', 35, 3.0)
+friend_three = Spy('ram', 'Mr.', 37, 5.5)
+
+# List of friends
+friends = [friend_one, friend_two, friend_three]
